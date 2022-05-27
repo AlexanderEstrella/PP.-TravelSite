bfc6d5af9bd02d9cd48343cbd21546f2cb30bffa
AlexanderME <97694971+AlexanderEstrella@users.noreply.github.com>
2022-05-27 09:14:52 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var places = [ "Maldives", "Spain", "Japan", "hongkong"];
+var random = Math.floor(Math.random() * 20) + 1;
+
+
+function randomPlace() {
+    if (random <= 5 && random > 1){
+        return places[0];
+    } else if (random <= 10 && random > 5 ) {
+        return places[1];
+    } else if (random <= 15 && random >10) {
+        return places[2];
+    } else if (random <= 20 && random > 15) {
+        return places[3];
+    }
+}
+
+document.getElementById('button').addEventListener('click', function () {
+   
+    var textbox = document.getElementById('name');
+   textbox.placeholder = randomPlace();
+   
+})
